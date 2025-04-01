5b94448f20101324b1210fb02f9a68d612f30254
Tushar Chuahan <imcatushar1994@gmail.com>
2025-04-01 23:06:08 +0530

    PB gmail add to send mail

--- src/utils/MailUtil.js
+++ src/utils/MailUtil.js
@@ -4,13 +4,13 @@ const sendingMail = async(to,subject,text)=>{
   const transporter = mailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'imcatushar1994@gmail.com',
-      pass: 'sdcl hqle sywb aglx'
+      user: 'pocketbuddy.food.offers@gmail.com',
+      pass: 'nieb lfyx vijj broj'
     }
   })
 
   const mailOptions = {
-    from: 'imcatushar1994@gmail.com',
+    from: 'pocketbuddy.food.offers@gmail.com',
     to: to,
     subject: subject,
     //text: text,
@@ -25,6 +25,45 @@ const sendingMail = async(to,subject,text)=>{
 //sendingMail("imcatushar1994@gmail.com","Welcome to Pocket-Buddy","this is test mail")
 
 module.exports ={ sendingMail };
+
+
+
+
+
+
+
+
+
+// const mailer = require('nodemailer');
+
+// const sendingMail = async(to,subject,text)=>{
+//   const transporter = mailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       user: 'imcatushar1994@gmail.com',
+//       pass: 'sdcl hqle sywb aglx'
+//     }
+//   })
+
+//   const mailOptions = {
+//     from: 'imcatushar1994@gmail.com',
+//     to: to,
+//     subject: subject,
+//     //text: text,
+//     html: text,
+//   }
+
+//   const  mailResponse = await transporter.sendMail(mailOptions);
+//   console.log(mailResponse);
+//   return mailResponse;
+// }
+
+// //sendingMail("imcatushar1994@gmail.com","Welcome to Pocket-Buddy","this is test mail")
+
+// module.exports ={ sendingMail };
+
+
+
 
 
 
